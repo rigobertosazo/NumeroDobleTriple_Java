6cfd8eaaf9e9ddcdcd635b634db8698e604e3f11
rigobertosazo <rigobertosazo@gmail.com>
2022-08-27 16:57:08 -0600

    Version 1.0

---
+++ src/numerodobletriple_java/NumeroDobleTriple_Java.java
@@ -0,0 +1,20 @@
+package numerodobletriple_java;
+import java.util.Scanner;
+
+/**
+ * @author Rib
+ */
+public class NumeroDobleTriple_Java {
+    /*Programa que lee un numeros entero por teclado y muestra el doble y triple del numero ingresado en pantalla*/
+    public static void main(String[] args) 
+    {
+        int numero;
+        Scanner s = new Scanner(System.in);
+            System.out.println("** 1. Programa que lee un numeros entero por teclado y muestra el doble y triple del numero ingresado en pantalla**\n");
+            System.out.println("Ingrese el primer numero: ");
+            numero=s.nextInt();
+            System.out.println("El numero ingresado es: "+numero);
+        System.out.println("El doble del numero ingresado es: "+numero*numero);
+        System.out.println("El triple del numero ingresado es: "+numero*numero*numero);
+    }  
+}
